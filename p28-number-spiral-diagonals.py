39414ed9a94ebc2de561ed8217abb2d97853ec51
zemanntru <truman.jian@mail.utoronto.ca>
2022-04-25 20:38:02 -0400

    solved problem 28

---
+++ p28-number-spiral-diagonals.py
@@ -0,0 +1,12 @@
+'''
+Suppose we want to find the sum of the values on the nth spiral, which has a side length of
+2n + 1. The sum of the four corners here are 4*(2n + 1)^2 - 2n - 2*2n - 3*2n
+= 4*(2n + 1)^2 - 12*n. Repeat this for 1001 spirals
+'''
+
+ans = 1
+mxn = (1001 - 1) // 2 + 1
+for x in range(1, mxn):
+    ans += (4*(2*x + 1)*(2*x + 1) - 12*x)
+
+print(ans)
