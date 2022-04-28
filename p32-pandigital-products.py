7038f5f58bacfc5469ad7148d575579af76fd9e8
zemanntru <truman.jian@mail.utoronto.ca>
2022-04-28 12:06:29 -0400

    solved problem 32

---
+++ p32-pandigital-products.py
@@ -0,0 +1,35 @@
+def next_permutation(perm):
+    for i in reversed(range(len(perm) - 1)):
+        if perm[i] < perm[i + 1]:
+            break
+    else:
+        return False
+
+    for j in reversed(range(i + 1, len(perm))):
+        if perm[i] < perm[j]:
+            break
+
+    perm[i], perm[j] = perm[j], perm[i]
+    perm[i + 1:] = reversed(perm[i + 1:])
+    return True
+
+perm, memo = [1,2,3,4,5,6,7,8,9], {}
+
+while True:
+    for i in range(1, len(perm) - 1):
+        for j in range(i + 1, len(perm) - i):
+            a = int(''.join(map(str,perm[:i])))
+            b = int(''.join(map(str,perm[i:j])))
+            c = int(''.join(map(str,perm[j:])))
+            if a*b == c:
+                if c not in memo:
+                    memo[c] = True
+
+    if not next_permutation(perm):
+        break
+
+ans = 0
+for prod in memo:
+    ans += prod
+
+print(ans)
