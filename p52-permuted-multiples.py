7c8cf0f8c276957250dc42166195d218c4cea31a
zemanntru <truman.jian@mail.utoronto.ca>
2022-07-11 21:06:38 -0400

    solved problem 52

--- p52-permuted-multiples.py
+++ p52-permuted-multiples.py
@@ -1,6 +1,9 @@
+'''
+The answer is a 6 digit number between 100000 and 167000. The lower limit is based on a 6 digit permutation requiring
+a 6 digit number. The upper limit is to ensure the multiple of 6 does not overflow.
+'''
 
 ans = 0
-
 for x in range(100000, 167000):
     bperm = True
     for n in [2,3,4,5,6]:
