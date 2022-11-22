7700385bb81078345f24338d77865ef0c15ac960
zemanntru <truman.jian@mail.utoronto.ca>
2022-11-22 01:33:08 -0500

    solved problem 78

--- p78-coin-partitions.py
+++ p78-coin-partitions.py
@@ -1,24 +1,24 @@
-sfac = [0] * 2
-def sumfac(x):
-    if x < len(sfac):
-        return sfac[x]
+'''
+We use Euler's recurrence for generating partitions. Let p(n) be the
+number of partitions for n, then the recursive process
+is p(n) = sum((-1)^(k + 1) * p(n - k(3k - 1)/2) for k in Z, k != 0). The lower
+bound for a non-zero p(n - k(3k - 1)/2) is -(sqrt(24*N + 1) - 1) / 6 and the
+upper bound would be (sqrt(24*N + 1) + 1) / 6. Since the answer wants modulo 
+1 million, it is congruent to taking the modulo at each stage of adding
+recurrent terms.
+'''
 
-    sfac.append(0)
-    if x == len(sfac):
-        print(x, len(sfac))
-        assert False
+import math
+mxn = mod = int(1e6)
+dp, N = [1, 1] + [0] * mxn, 2
 
-    n, p, ret = x, 2, 0
-    while n > 1:
-        if n % p == 0:
-            sfac[x] += p
-            while n % p == 0:
-                n /= p
+while dp[N - 1] != 0:
+    lo = -int(math.ceil((math.sqrt(24*N + 1) - 1) / 6))
+    hi = int(math.floor((math.sqrt(24*N + 1) + 1) / 6)) + 1
+    for x in range(lo, hi):
+        if x != 0:
+            dp[N] = (dp[N] + (-1 if x % 2 == 0 else 1) * dp[N - x*(3*x - 1)//2]) % mod
 
-        if n == 1: break
-        if p*p > n:
-            sfac[x] += n
-            break
-        p += 1
+    N += 1
 
-    return sfac[x]
+print('The value of n such that p(n) is divisible by 1 million: {0}'.format(N - 1))
